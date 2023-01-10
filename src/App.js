3bb60308e0a9124ee04f79e957987ec89c4d8c03
Nurbaeva Assol <nurbaevaassol@gmail>
2023-01-10 22:53:46 +0300

    lesson5project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
-         </div>
+      <p>qqqqq</p>
+    </div>
   );
 }
 
